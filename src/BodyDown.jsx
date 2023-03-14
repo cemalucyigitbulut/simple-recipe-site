b34fa664f1e240adfbaa6a77db4a5db69f2d5096
YilanCemal <cemalyigit123@hotmail.com>
2023-03-14 23:11:35 +0300

    card eklendi

--- src/BodyDown.jsx
+++ src/BodyDown.jsx
@@ -3,12 +3,14 @@ import "./BodyDown.css";
 
 const BodyDown = () => {
   return (
+    <div className="BodyDown">
+    <h1>Some Recipes</h1>
     <div className="bodydown-container">
       <div class="container">
         <div class="card">
           <img src="images/salad.jpg" />
           <div class="card__content">
-            <h2>Web Design</h2>
+          <h2>Web Design</h2>
             <p>
               Lorem Ipsum has been the industry's standard dummy text ever since
               the 1500s.
@@ -17,6 +19,156 @@ const BodyDown = () => {
           </div>
         </div>
       </div>
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container">
+        <div class="card">
+          <img src="images/salad.jpg" />
+          <div class="card__content">
+          <h2>Web Design</h2>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
     </div>
   );
 };
