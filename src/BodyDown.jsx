6521f256b08a5785be2a46e8a0599ab116cea40f
YilanCemal <cemalyigit123@hotmail.com>
2023-03-16 14:28:50 +0300

    card tamam

--- src/BodyDown.jsx
+++ src/BodyDown.jsx
@@ -1,164 +1,98 @@
 import React from "react";
 import "./BodyDown.css";
+import CardInside from "./CardInside";
 
 const BodyDown = () => {
   return (
     <div className="BodyDown">
-    <h1>What to Cook Tonight</h1>
-    <p>Fast, fresh, and foolproof</p>
-    <div className="bodydown-container">
-      <div class="container">
-        <div class="card">
-          <img src="images/Noodles.jpg" />
-          <div className="card-title"><h2>Peanut Noodles</h2></div>
-          <div class="card__content">
-            <p>
-            These peanut butter noodles have “a depth of flavor and dimension that belies what a fast, easy, flexible.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
+      <h1>What to Cook Tonight</h1>
+      <p>Fast, fresh, and foolproof</p>
+      <div className="bodydown-container">
+        <CardInside
+          src="images/Noodles.jpg"
+          text="These peanut butter noodles have “a depth of flavor and
+                dimension that belies what a fast, easy, flexible."
+          title="Peanut Noodles"
+          path="/"
+        />
+
+        <CardInside
+          src="images/burger.jpg"
+          text="Dry burgers? We dont know them..."
+          title="Juicier Turkey"
+          path="/"
+        />
+
+        <CardInside
+          src="images/Spicy.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Spicy Rigatoni"
+          path="/"
+        />
+
+<CardInside
+          src="images/deviled.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Deviled Eggs"
+          path="/"
+        />
+
+<CardInside
+          src="images/sticky ricel.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Mango Sticky Rice"
+          path="/"
+        />
+
+
+<CardInside
+          src="images/pad thai.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Pad Thai"
+          path="/"
+        />
+
+
+<CardInside
+          src="images/Matzo.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Matzo Brei"
+          path="/"
+        />
+
+
+<CardInside
+          src="images/pasta.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Simple Pasta"
+          path="/"
+        />
+
+
+<CardInside
+          src="images/sloppy.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Sloppy Joes"
+          path="/"
+        />
+
+
+<CardInside
+          src="images/lasagna.jpg"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt."
+          title="Ricotta Lasagna"
+          path="/"
+        />
+
       </div>
-
-      <div class="container">
-        <div class="card">
-          <img src="images/burger.jpg" />
-          <div className="card-title"><h2>Juicier Turkey</h2></div>
-          <div class="card__content">
-            <p>
-             Dry burgers? We dont know them..
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/Pea Chowder.jpg" />
-          <div className="card-title"><h2>Pea Chowder</h2></div>
-          <div class="card__content">
-            <p>
-            While winter calls for bowls of fully loaded baked potato soup.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="card">
-          <img src="images/Spicy.jpg" />
-          <div className="card-title"><h2>Spicy Rigatoni</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/deviled.jpg" />
-          <div className="card-title"><h2>Deviled Eggs</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/sticky ricel.jpg" />
-          <div className="card-title"><h2>Mango Sticky Rice</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/pad thai.jpg" />
-          <div className="card-title"><h2>Pad Thai</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="card">
-          <img src="images/Matzo.jpg" />
-          <div className="card-title"><h2>Matzo Brei</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="card">
-          <img src="images/pasta.jpg" />
-          <div className="card-title"><h2>Simple Pasta</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/sloppy.jpg" />
-          <div className="card-title"><h2>Sloppy Joes</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="container">
-        <div class="card">
-          <img src="images/lasagna.jpg" />
-          <div className="card-title"><h2>Ricotta Lasagna</h2></div>
-          <div class="card__content">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-            <a href="#">Read More</a>
-          </div>
-        </div>
-      </div>
-
-
-
-    </div>
     </div>
   );
 };
---
+++ src/CardInside.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./CardInside.css"
+
+const CardInside = (props) => {
+  return (
+    <>
+      <div class="container">
+        <div class="card">
+          <img src={props.src}/>
+          <div className="card-title">{props.title}</div>
+          <div class="card__content">
+            <p>
+           {props.text}
+            </p>
+            <Link to={props.path}>Read More</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CardInside
